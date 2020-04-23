18408a7945180de9b6377fbda92c6a3e1a93fc7f
Ethan <38301917+eb7931@users.noreply.github.com>
2020-04-22 23:43:36 -0500

    Gui changes, moved the main function to main

--- src/assignment/Game.java
+++ src/assignment/Game.java
@@ -63,12 +63,8 @@ public class Game {
 	}
 	
 	public static void initializeGUI() {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				gui = new GUI();
-				gui.frame.setVisible(true);
-			}
-		});
+		gui = new GUI();
+		gui.frame.setVisible(true);
 	}
 	public GUI getGUI() {
 		return gui;
@@ -78,18 +74,5 @@ public class Game {
 		return round;
 	}
 	
-	// Starts the game
-	public static void main(String[] args) {
-		Game game = new Game();
-		
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-					gui = new GUI();
-					gui.frame.setVisible(true);
-					game.round.promptPlay(players, gui);
-			}
-		});
-
-	}
 
 }
